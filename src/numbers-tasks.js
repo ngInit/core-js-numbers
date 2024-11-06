dd7201bb9c627e7b115299560b135dd55e06c31c
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-11-06 11:46:13 +0100

    feat: solve getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -256,8 +256,11 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0 || index === 1) {
+    return index;
+  }
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
